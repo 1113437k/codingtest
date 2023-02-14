071bcccea7bc57b3759ee70fcdcdc4b1d44e166b
1113437k <insort@naver.com>
2023-02-14 12:35:11 +0900

    fix : add code

--- Coding-test-java/src/com/coding/test/practice/OneDigit.java
+++ Coding-test-java/src/com/coding/test/practice/OneDigit.java
@@ -1,2 +1,11 @@
-package com.coding.test.practice;public class OneDigit {
+package com.coding.test.practice;
+
+public class OneDigit {
+    public static void main(String[] args) {
+        int n = 12345;
+        while (n > 0) {
+            System.out.println(n % 10);
+            n /= 10;
+        }
+    }
 }
